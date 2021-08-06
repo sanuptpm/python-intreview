3f96815eb6f5e6b5d74514216604f9f2d0f0eeaa
sanu <sanuptpm@gmail.com>
2021-08-06 22:34:46 +0530

    commit

--- README.md
+++ README.md
@@ -4,15 +4,21 @@ https://pynative.com/python-exercises-with-solutions/
 
 https://pynative.com/python/quizzes/
 
+#main
+
+https://www.programiz.com/python-programming/object-oriented-programming
+
+https://www.edureka.co/blog/object-oriented-programming-python/
+
+############################
+
 https://pynative.com/useful-python-tips-and-tricks-every-programmer-should-know/
 
 https://www.geeksforgeeks.org/object-oriented-programming-in-python-set-2-data-hiding-and-object-printing/?ref=rp
 
 https://www.pythonprogramming.in/object-oriented-programming.html
 
-https://www.programiz.com/python-programming/object-oriented-programming
 
-https://www.edureka.co/blog/object-oriented-programming-python/
 
 https://www.listendata.com/2019/08/python-object-oriented-programming.html?m=1
 
--- main.py
+++ main.py
@@ -7,7 +7,56 @@ class RangeExample:
         for i in range(2, 10):
             print("range_start_stop", i)
 
+    def range_step(self):
+        for i in range(0,10,2):
+            print("range_step", i)
+
+    def range_reverse(self):
+        for i in range(10,0,-2):
+            print("range_step", i)
+
+    def reversed_range(self):
+        for i in reversed(range(0,10,1)):
+            print("range_step", i)
+
+
+class Animal:
+    def __init__(self, Animal):
+        print(Animal, 'is an animal.');
+
+
+class Mammal(Animal):
+    def __init__(self, mammalName):
+        print(mammalName, 'is a warm-blooded animal.')
+        super().__init__(mammalName)
+
+
+class NonWingedMammal(Mammal):
+    def __init__(self, NonWingedMammal):
+        print(NonWingedMammal, "can't fly.")
+        super().__init__(NonWingedMammal)
+
+
+class NonMarineMammal(Mammal):
+    def __init__(self, NonMarineMammal):
+        print(NonMarineMammal, "can't swim.")
+        super().__init__(NonMarineMammal)
+
+
+class Dog(NonMarineMammal, NonWingedMammal):
+    def __init__(self):
+        print('Dog has 4 legs.');
+        super().__init__('Dog')
+
+
+d = Dog()
+print('')
+bat = NonMarineMammal('Bat')
+
 
 obj1 = RangeExample()
-obj1.range_method()
-obj1.range_start_stop()
+# obj1.range_method()
+# obj1.range_start_stop()
+# obj1.range_step()
+obj1.reversed_range()
+
